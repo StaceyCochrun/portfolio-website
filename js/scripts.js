14b18f80b60611cd193b5da327256e6316ab1f43
StaceyCochrun <115327963+StaceyCochrun@users.noreply.github.com>
2022-10-17 14:29:14 -0700

    hamburger icon addition & styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 console.log("Hi! Welcome to My Portfolio Site")
+
+function menuToggle() {
+}
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
